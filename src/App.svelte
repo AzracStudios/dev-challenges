5b8b9cda091b77c36d89f7d5d94024db5e0e93f1
AzracStudios <azracstudios@gmail.com>
2022-09-18 21:31:17 +0530

    Init

---
+++ src/App.svelte
@@ -0,0 +1,132 @@
+<script>
+  import Button from "./button.svelte";
+</script>
+
+<div class="app">
+  <h1 style="color: rgb(50, 50, 50);">Buttons</h1>
+
+  <div class="row">
+    <div class="cont">
+      <code>{"<Button />"}</code>
+      <Button text="Default" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="cont">
+      <code>{'<Button variant="outline" />'}</code>
+      <Button text="Default" variant="outline" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button variant="text" />'}</code>
+      <Button text="Default" variant="text" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="cont">
+      <code>{"<Button disableShadow />"}</code>
+      <Button text="Default" color="primary" disableShadow />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="cont">
+      <code>{"<Button disabled />"}</code>
+      <Button text="Default" disabled />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button variant="text" disabled />'}</code>
+      <Button text="Default" variant="text" disabled />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="cont">
+      <code>{'<Button startIcon="local_grocery_store" />'}</code>
+      <Button text="Default" startIcon="local_grocery_store" color="primary" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button endIcon="local_grocery_store" />'}</code>
+      <Button text="Default" endIcon="local_grocery_store" color="primary" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="cont">
+      <code>{'<Button size="sm" />'}</code>
+      <Button text="Default" color="primary" size="sm" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button size="md" />'}</code>
+      <Button text="Default" color="primary" size="md" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button size="lg" />'}</code>
+      <Button text="Default" color="primary" size="lg" />
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="cont">
+      <code>{'<Button color="default" />'}</code>
+      <Button text="Default" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button color="primary" />'}</code>
+      <Button text="Primary" color="primary" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button color="secondary" />'}</code>
+      <Button text="Secondary" color="secondary" />
+    </div>
+
+    <div class="cont">
+      <code>{'<Button color="danger" />'}</code>
+      <Button text="Danger" color="danger" />
+    </div>
+  </div>
+
+  <div class="spacer" />
+</div>
+
+<style lang="scss">
+  * {
+    margin: 0px;
+    padding: 0px;
+
+    &:not(code) {
+      font-family: Poppins;
+    }
+
+    box-sizing: border-box;
+  }
+
+  .spacer {
+    height: 100px;
+  }
+
+  .app {
+    display: flex;
+    flex-direction: column;
+    margin-left: 20px;
+    gap: 50px;
+  }
+  .row {
+    display: flex;
+    gap: 50px;
+  }
+
+  .cont {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
---
+++ src/button.svelte
@@ -0,0 +1,143 @@
+<script>
+  export let text = "Button";
+  export let variant;
+  export let disableShadow;
+  export let disabled;
+  export let startIcon;
+  export let endIcon;
+  export let size = "md";
+  export let color = "default";
+</script>
+
+<div
+  class={`button ${variant} ${size} ${color} ${disabled ? "disabled" : ""} ${
+    disableShadow || disabled || variant ? "" : "shadow"
+  }`}
+>
+  <span class={`material-symbols-outlined ${startIcon ? "" : "hidden"}`}
+    >{startIcon}</span
+  >
+  {text}
+  <span class={`material-symbols-outlined ${endIcon ? "" : "hidden"}`}
+    >{endIcon}</span
+  >
+</div>
+
+<style lang="scss">
+  .material-symbols-outlined {
+    font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 48;
+  }
+
+  .hidden {
+    display: none;
+  }
+
+  .button {
+    &.sm {
+        padding: 6px 12px;
+    }
+
+    &.md {
+        padding: 8px 16px;
+    }
+
+    &.lg {
+        padding: 11px 22px;
+    }
+
+    width: fit-content;
+    border-radius: 6px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:not(.disabled) {
+      &:hover,
+      &:focus {
+        background: rgba(174, 174, 174, 1);
+      }
+    }
+
+    &.shadow {
+      box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    }
+
+    &.disabled {
+      &:not(.text) {
+        background: rgba(224, 224, 224, 1);
+      }
+
+      color: rgba(158, 158, 158, 1);
+    }
+  }
+
+  .default {
+    background: rgba(224, 224, 224, 1);
+    color: rgba(63, 63, 63, 1);
+
+    &:not(.disabled) {
+      &:hover,
+      &:focus {
+        background: rgba(174, 174, 174, 1);
+      }
+    }
+  }
+
+  .primary {
+    background: rgba(41, 98, 255, 1);
+    color: white;
+
+    &:not(.disabled) {
+      &:hover,
+      &:focus {
+        background: rgba(0, 57, 203, 1);
+      }
+    }
+  }
+
+  .secondary {
+    background: rgba(69, 90, 100, 1);
+    color: white;
+
+    &:not(.disabled) {
+      &:hover,
+      &:focus {
+        background: rgba(28, 49, 58, 1);
+      }
+    }
+  }
+
+  .danger {
+    background: rgba(211, 47, 47, 1);
+    color: white;
+
+    &:not(.disabled) {
+      &:hover,
+      &:focus {
+        background: rgba(154, 0, 7, 1);
+      }
+    }
+  }
+
+  .outline {
+    background: white;
+    border: 1px solid rgba(61, 90, 254, 1);
+    color: rgba(61, 90, 254, 1);
+
+    &:hover,
+    &:focus {
+      background: rgba(41, 98, 255, 0.1);
+    }
+  }
+
+  .text {
+    background: white;
+    border: 1px solid transparent;
+    color: rgba(61, 90, 254, 1);
+  }
+</style>
