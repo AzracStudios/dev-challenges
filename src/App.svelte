b5ead60332d29a2183708f90632f1cbdfbe8ae4e
AzracStudios <azracstudios@gmail.com>
2022-10-05 16:09:37 +0530

    Change fetch version on API

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,29 @@
 <script>
   import Random from "./components/random.svelte";
-  import { randomQuote, authorQuote } from "quotegarden";  
+  import { randomQuote, authorQuote } from "./quotes";
   import Quote from "./components/quote.svelte";
   import { onMount } from "svelte";
   import Author from "./components/author.svelte";
 
-  $:quote = "";
-  $:author = "";
-  $:genre = "";
-
-  $:view = "quote";
+  $: quote = "";
+  $: author = "";
+  $: genre = "";
 
   const getNewQuote = () => {
-	randomQuote()
-	.then((q) => {
-		quote = q.data[0].quoteText;
-		author = q.data[0].quoteAuthor;
-		genre = q.data[0].quoteGenre;
-	})
-  }
+    randomQuote().then((q) => {
+      quote = q.data[0].quoteText;
+      author = q.data[0].quoteAuthor;
+      genre = q.data[0].quoteGenre;
+    });
+  };
 
-  onMount(getNewQuote)
+  onMount(getNewQuote);
 </script>
 
 <div class="app">
-  <Random handleClick={getNewQuote}/>
-  <Quote {quote}/>
-  <Author {author} {genre}></Author>
+  <Random handleClick={getNewQuote} />
+  <Quote {quote} />
+  <Author {author} {genre} />
 </div>
 
 <style lang="scss">
@@ -34,18 +31,17 @@
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
-	font-family: Poppins;
+    font-family: Poppins;
   }
 
   .app {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: flex-start;
-	height: 100vh;
-	width: fit-content;
-	margin-left: auto;
-	margin-right: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    height: 100vh;
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
   }
-
 </style>
